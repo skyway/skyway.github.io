052ef600b21bdbfcbde0bc67a58ada15bf832969
Ryosuke Otsuya <r.otsuya@gmail.com>
2017-08-28 12:13:38 +0900

    remove unnecessary window.onload

--- js/script.js
+++ js/script.js
@@ -1,72 +1,65 @@
-window.onload = function () {
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 
-    $(function () {
-      // enable tooltip
-      $('[data-toggle="tooltip"]').tooltip();
+  // enable toggle icon for collapse
+  initCollapseIconToggle();
 
-      // enable toggle icon for collapse
-      initCollapseIconToggle();
+  /** 新着情報取得用スクリプト ここから**/
 
+  // 言語判定（英語ページの場合は英語の情報を取得するため）
+  var href = window.location.href;
+  var prefix = 'ja';
+  if(href.match('\/en')){
+    prefix = 'en-us';
+  }
 
-      /** 新着情報取得用スクリプト ここから**/
+  // APIリクエスト
+  // リクエスト条件：最大取得件数4件、作成日で降順ソート
+  const announce = 'https://skyway-support.zendesk.com/api/v2/help_center/'+prefix+'/sections/207255008/articles.json?sort_by=created_at&sort_order=desc&per_page=3';
+  const maintenance = 'https://skyway-support.zendesk.com/api/v2/help_center/'+prefix+'/sections/207271047/articles.json?sort_by=created_at&sort_order=desc&per_page=3';
+  const failure = 'https://skyway-support.zendesk.com/api/v2/help_center/'+prefix+'/sections/207255108/articles.json?sort_by=created_at&sort_order=desc&per_page=3';
 
-      // 言語判定（英語ページの場合は英語の情報を取得するため）
-      var href = window.location.href;
-      var prefix = 'ja';
-      if(href.match('\/en')){
-        prefix = 'en-us';
-      }
+  //新着情報サイトURL
+  const announce_site = 'https://support.skyway.io/hc/'+prefix+'/sections/207255008';
+  const maintenance_site = 'https://support.skyway.io/hc/'+prefix+'/sections/207271047';
+  const failure_site = 'https://support.skyway.io/hc/'+prefix+'/sections/207255108';
 
-      // APIリクエスト
-      // リクエスト条件：最大取得件数4件、作成日で降順ソート
-      const announce = 'https://skyway-support.zendesk.com/api/v2/help_center/'+prefix+'/sections/207255008/articles.json?sort_by=created_at&sort_order=desc&per_page=3';
-      const maintenance = 'https://skyway-support.zendesk.com/api/v2/help_center/'+prefix+'/sections/207271047/articles.json?sort_by=created_at&sort_order=desc&per_page=3';
-      const failure = 'https://skyway-support.zendesk.com/api/v2/help_center/'+prefix+'/sections/207255108/articles.json?sort_by=created_at&sort_order=desc&per_page=3';
+  $.ajax({
+    url: announce,
+    type: 'GET',
+    dataType: 'json',
+    async: 'true'
+  }).done(function(data) {
+    updateNews(data,'announce',announce_site);
+  }).fail(function(data) {
+    console.log('xhr failed');
+  });
 
-      //新着情報サイトURL
-      const announce_site = 'https://support.skyway.io/hc/'+prefix+'/sections/207255008';
-      const maintenance_site = 'https://support.skyway.io/hc/'+prefix+'/sections/207271047';
-      const failure_site = 'https://support.skyway.io/hc/'+prefix+'/sections/207255108';
+  $.ajax({
+    url: maintenance,
+    type: 'GET',
+    dataType: 'json',
+    async: 'true'
+  }).done(function(data) {
+    updateNews(data,'maintenance',maintenance_site);
+  }).fail(function(data) {
+    console.log('xhr failed');
+  });
 
-      $.ajax({
-        url: announce,
-        type: 'GET',
-        dataType: 'json',
-        async: 'true'
-      }).done(function(data) {
-        updateNews(data,'announce',announce_site);
-      }).fail(function(data) {
-        console.log('xhr failed');
-      });
+  $.ajax({
+    url: failure,
+    type: 'GET',
+    dataType: 'json',
+    async: 'true'
+  }).done(function(data) {
+    updateNews(data,'failure',failure_site);
+  }).fail(function(data) {
+    console.log('xhr failed');
+  });
 
-      $.ajax({
-        url: maintenance,
-        type: 'GET',
-        dataType: 'json',
-        async: 'true'
-      }).done(function(data) {
-        updateNews(data,'maintenance',maintenance_site);
-      }).fail(function(data) {
-        console.log('xhr failed');
-      });
+});
 
-      $.ajax({
-        url: failure,
-        type: 'GET',
-        dataType: 'json',
-        async: 'true'
-      }).done(function(data) {
-        updateNews(data,'failure',failure_site);
-      }).fail(function(data) {
-        console.log('xhr failed');
-      });
-
-    });
-
-  /** 新着情報取得用スクリプト ここまで **/
-
-
-};
+/** 新着情報取得用スクリプト ここまで **/
 
 // 最新情報のDom生成
 function updateNews(obj,id,siteurl){
